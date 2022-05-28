a756e31a5bf86568d787b0955a079d7b39a4ecff
DhruviPatel2009 <87831386+DhruviPatel2009@users.noreply.github.com>
2022-05-28 13:53:34 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -10,15 +10,26 @@ recognition.onresult=function(event){
     console.log(event);
     var content=event.results[0][0].transcript;
     console.log(content);
+    if(content=="take my selfie")
+    {
+        console.log("taking selfie---");
+        speak();
+    }
     document.getElementById("textbox").innerHTML=content;
 }
 
 function speak(){
     var synth= window.speechSynthesis;
-    speak_data= document.getElementById("textbox").value;
+    speak_data="taking your selfie in 5 seconds";
+    
     var utterThis=new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
     Webcam.attach(camera);
+
+    setTimeout(function()  {
+        take_snapshot();
+        save();
+    }, 5000);
 }
 
 camera=document.getElementById("camera");
@@ -28,3 +39,15 @@ Webcam.set({
     image_format:'jpeg',
     jpeg_quality:90
 });
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="selfie_image" src="'+data_uri+'">';
+    });
+}
+
+function save(){
+    link=document.getElementById("link");
+    image=document.getElementById("selfie_image").src;
+    link.click();
+}
